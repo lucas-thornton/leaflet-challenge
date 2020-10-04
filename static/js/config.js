e3c0aee97da91d00d6a5567ef44186af05e8268b
lucas-thornton <le.thornton644@gmail.com>
2020-10-04 23:58:07 +0800

    update

--- static/js/config.js
+++ static/js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = "pk.eyJ1IjoibHRob3JudG9uIiwiYSI6ImNrZmR1MzdvbTA5dHQyc212OGplaTVodHgifQ.8uKrqQURDNspdB4UUq8ULw";
+const API_KEY = "pk.eyJ1IjoibHRob3JudG9uIiwiYSI6ImNrZmR1NW4xMTF0OTcycW1xN2Z0ZmFtcHEifQ.FakbHA1Riey-OOJHNOfJ-A";
--- static/js/logic.js
+++ static/js/logic.js
@@ -1,11 +1,11 @@
 // Creating map object
 var myMap = L.map("mapid", {
-  center: [37.09, -95.71],
-  zoom: 5
+  center: [70, 0],
+  zoom: 3
 });
   
   // tile layer
-  L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}", {
+  var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     tileSize: 512,
     maxZoom: 18,
@@ -14,91 +14,97 @@ var myMap = L.map("mapid", {
     accessToken: API_KEY
   }).addTo(myMap);
 
-  // retrieve earthquake geoJSON data.
-d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson", function(data) {
 
-// get colour with depths and radius with magnitude
-  function styleInfo(feature) {
-    return {
-      opacity: 1,
-      fillOpacity: 1,
-      fillColor: getColor(feature.geometry.coordinates[2]),
-      color: "#000000",
-      radius: getRadius(feature.properties.mag),
-      stroke: true,
-      weight: 0.5
-    };
+// var baseMaps = {
+//     "Light Map": lightmap
+//   };  
+
+// Create a layer control, pass in the baseMaps and overlayMaps. Add the layer control to the map
+// L.control.layers(baseMaps, overlayMaps, {
+//   collapsed: false
+// }).addTo(myMap);
+
+  // Create an overlayMaps object to hold the quakes_occur layer
+  // var overlayMaps = {
+  //   "Earthquakes": quakes_occur
+  // };
+
+  // Create a layer control, pass in the baseMaps and overlayMaps. Add the layer control to the map
+  // L.control.layers(baseMaps, overlayMaps, {
+  //   collapsed: false
+  // }).addTo(map);
+
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_week.geojson", getMarkers);
+
+function fillColour(data) {
+  switch (true) {
+    case data > 100:
+      return "#ea3c5c";
+    case data > 80:
+      return "#ea825c";
+    case data > 60:
+      return "#ee8c01";
+    case data > 40:
+      return "#eecc00";
+    case data > 20:
+      return "#d5ee00";
+    default:
+      return "#97ee00";
   }
+}
 
-  // color of the marker based on the depth intervals
-  function getColor(depth) {
-    switch (true) {
-      case depth > 90:
-        return "#ea2c2c";
-      case depth > 70:
-        return "#ea822c";
-      case depth > 50:
-        return "#ee9c00";
-      case depth > 30:
-        return "#eecc00";
-      case depth > 10:
-        return "#d4ee00";
-      default:
-        return "#98ee00";
-    }
-  }
+function getMarkers(response) {
 
-  // size of circles depending of the magnitude
+  var quake_data = response.features;
+  var quakeMarkers = [];
+  // var quakeMarkers = L.markerClusterGroup();
+  for (var index = 0; index < quake_data.length; index++) {
+        var quake = quake_data[index];
+        // quakeMarkers.addLayer(L.marker([quake.geometry.coordinates[0], quake.geometry.coordinates[1]], radius = quake.properties.mag, fillColor = "#ea2c2c"))
+        var quakeMarker = L.circleMarker([quake.geometry.coordinates[0], quake.geometry.coordinates[1]], {radius: quake.properties.mag*5, fillColor: fillColour(quake.geometry.coordinates[2])})
+      .bindPopup("<h3>" + quake.properties.title + "<h3><h3>Magnitude: " + quake.properties.mag + "</h3>");
+      
+      quakeMarkers.push(quakeMarker);
 
-  function getRadius(magnitude) {
-    if (magnitude === 0) {
-      return 1;
-    }
+      // console.log(quakeMarkers)
+  };
+L.layerGroup(quakeMarkers).addTo(myMap);
+// console.log(quakeMarkers[8])
+// myMap.addLayer(quakeMarkers);
 
-    return magnitude * 3;
-  }
+};
 
-  // add GeoJSON layer to the map
-  L.geoJson(data, {
-    pointToLayer: function(feature, latlng) {
-      return L.circleMarker(latlng);
-    },
-    style: styleInfo,
-    onEachFeature: function(feature, layer) {
-      layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
-    }
+var legend = L.control({ position: "bottomright" });
+legend.onAdd = function() {
+  var div = L.DomUtil.create("div", "info legend");
+  var limits = [-20, 20, 40, 60, 80, 100];
+  var colors = ["#97ee00", "#d5ee00", "#eecc00", "#ee8c01", "#ea825c", "#ea3c5c", ];
+  var labels = [];
 
-  }).addTo(myMap);
+  // Add min & max
+  var legendInfo = "<h1>Quake Depth</h1>" +
+    "<div class=\"labels\">" +
+      "<div class=\"min\">" + limits[0] + "</div>" +
+      "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
+    "</div>";
 
-  var legend = L.control({
-    position: "bottomright"
+  div.innerHTML = legendInfo;
+
+  limits.forEach(function(limit, index) {
+    labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
   });
 
+  div.innerHTML += "<ul>" + labels.join("") + "</ul>";
+  return div;
+};
 
-  legend.onAdd = function() {
-    var div = L
-      .DomUtil
-      .create("div", "info legend");
-
-    var grades = [-10, 10, 30, 50, 70, 90];
-    var colors = [
-      "#98ee00",
-      "#d4ee00",
-      "#eecc00",
-      "#ee9c00",
-      "#ea822c",
-      "#ea2c2c"
-    ];
+// Adding legend to the map
+legend.addTo(myMap);
 
 
-    for (var i = 0; i < grades.length; i++) {
-      div.innerHTML += "<i style='background: " + colors[i] + "'></i> " +
-        grades[i] + (grades[i + 1] ? "&ndash;" + grades[i + 1] + "<br>" : "+");
-    }
-    return div;
-  };
 
 
-  legend.addTo(myMap);
 
-});
+
+// Perform an API call to the Citi Bike API to get quake information. Call createMarkers when complete
+// d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson", createMarkers);
